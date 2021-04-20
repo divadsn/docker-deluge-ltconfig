df4c562e42b595ec684909dc0dd6ae99f0cba04b
David Sn <divad.nnamtdeis@gmail.com>
2021-04-20 19:49:14 +0200

    Add entrypoint script to handle user and file permissions
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+id -u $USER &>/dev/null
+
+# Create user if not exists
+if [ "$?" -ne 0 ]; then
+    addgroup -g $GID -S $USER
+    adduser -S -D -u $UID -s /sbin/nologin -G $USER -g $USER $USER
+    chown -R $USER:$USER /config /data
+fi
+
+# Copy latest ltConfig plugin to /config/plugins
+if [ ! -d "/config/plugins" ]; then
+    cp -rp /usr/share/deluge/plugins /config/plugins
+    chown -R $USER:$USER /config/plugins
+else
+    rm -f /config/plugins/ltConfig-*.egg && cp -p /usr/share/deluge/plugins/ltConfig-*.egg /config/plugins
+    chown $USER:$USER /config/plugins/ltConfig-*.egg
+fi
+
+# Run deluge
+su $USER -s /bin/sh -c run_deluge.sh
